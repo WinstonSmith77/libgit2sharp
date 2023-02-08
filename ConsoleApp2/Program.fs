fc058658f41f251b4186a4de35c83eaf146b20a8
Henning, Matthias <M.Henning@infas-LT.de>
2023-02-08 14:35:04 +0100

    testcode

---
+++ ConsoleApp2/Program.fs
@@ -0,0 +1,14 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+open LibGit2Sharp
+
+printfn "Hello from F#"
+
+let folder = "C:\Users\Henning\Desktop\einarbeitung\pdf"// Util.CurrentQueryPath |> Path.GetDirectoryName 
+
+let identity = new Identity("Matthias Henning", "matze_henning@gmx.de")        
+let createSignature () = new Signature(identity, DateTimeOffset.Now);
+
+let repo = new Repository(folder)
+let commits = repo.Commits.QueryBy("skia/fonts.linq") |> List.ofSeq
